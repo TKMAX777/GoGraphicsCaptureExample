d475d0fc5a9d8fff2ee83f3dbd313fac844bca29
TKMAX777 <service@tk-max.com>
2023-03-21 18:17:27 +0900

    update

--- capture.go
+++ capture.go
@@ -14,6 +14,7 @@ import (
 
 type CaptureHandler struct {
 	device                 *winrt.IDirect3DDevice
+	deviceDx               *dx11.ID3D11Device
 	graphicsCaptureItem    *winrt.IGraphicsCaptureItem
 	framePool              *winrt.IDirect3D11CaptureFramePool
 	graphicsCaptureSession *winrt.IGraphicsCaptureSession
@@ -21,12 +22,14 @@ type CaptureHandler struct {
 }
 
 func (c *CaptureHandler) StartCapture(hwnd win.HWND) error {
+	// Initialize Windows Runtime
 	err := winrt.RoInitialize(winrt.RO_INIT_MULTITHREADED)
 	if err != nil {
 		return errors.Wrap(err, "RoInitialize")
 	}
 	defer winrt.RoUninitialize()
 
+	// Create capture device
 	var featureLevels = []dx11.D3D_FEATURE_LEVEL{
 		dx11.D3D_FEATURE_LEVEL_11_0,
 		dx11.D3D_FEATURE_LEVEL_10_1,
@@ -36,34 +39,40 @@ func (c *CaptureHandler) StartCapture(hwnd win.HWND) error {
 		dx11.D3D_FEATURE_LEVEL_9_1,
 	}
 
-	var device *dx11.ID3D11Device
 	err = dx11.D3D11CreateDevice(
-		nil, dx11.D3D_DRIVER_TYPE_HARDWARE, 0, dx11.D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+		nil, dx11.D3D_DRIVER_TYPE_HARDWARE, 0, dx11.D3D11_CREATE_DEVICE_BGRA_SUPPORT|dx11.D3D11_CREATE_DEVICE_DEBUG,
 		&featureLevels[0], len(featureLevels),
-		dx11.D3D11_SDK_VERSION, &device, nil, nil,
+		dx11.D3D11_SDK_VERSION, &c.deviceDx, nil, nil,
 	)
 	if err != nil {
 		return errors.Wrap(err, "D3DCreateDevice")
 	}
 
+	var context = c.deviceDx.GetImmediateContext()
+	defer context.Release()
+
+	// Query interface of DXGIDevice
 	var dxgiDevice *dx11.IDXGIDevice
-	err = device.PutQueryInterface(dx11.IDXGIDeviceID, &dxgiDevice)
+	err = c.deviceDx.PutQueryInterface(dx11.IDXGIDeviceID, &dxgiDevice)
 	if err != nil {
 		return errors.Wrap(err, "PutQueryInterface")
 	}
 
 	var deviceRT *ole.IInspectable
 
+	// convert D3D11Device(Dx11) to Direct3DDevice(WinRT)
 	err = dx11.CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice, &deviceRT)
 	if err != nil {
 		return errors.Wrap(err, "CreateDirect3D11DeviceFromDXGIDevice")
 	}
 
+	// Query interface of IDirect3DDevice
 	err = deviceRT.PutQueryInterface(winrt.IDirect3DDeviceID, &c.device)
 	if err != nil {
 		return errors.Wrap(err, "QueryInterface: IDirect3DDeviceID")
 	}
 
+	// Create Capture Settings
 	factory, err := ole.RoGetActivationFactory(winrt.GraphicsCaptureItemClass, winrt.IGraphicsCaptureItemInteropID)
 	if err != nil {
 		return errors.Wrap(err, "RoGetActivationFactory: IGraphicsCaptureItemID")
@@ -76,11 +85,15 @@ func (c *CaptureHandler) StartCapture(hwnd win.HWND) error {
 	}
 
 	var captureItemDispatch *ole.IInspectable
-	// err = interop.CreateForWindow(hwnd, winrt.IGraphicsCaptureItemID, &captureItemDispatch)
-	// if err != nil {
-	// 	return errors.Wrap(err, "CreateForWindow")
-	// }
+	/*
+		// Capture for the window specified
+		err = interop.CreateForWindow(hwnd, winrt.IGraphicsCaptureItemID, &captureItemDispatch)
+		if err != nil {
+			return errors.Wrap(err, "CreateForWindow")
+		}
+	*/
 
+	// Capture for the monitor specified
 	var hmoni = win.MonitorFromWindow(hwnd, win.MONITORINFOF_PRIMARY)
 
 	err = interop.CreateForMonitor(hmoni, winrt.IGraphicsCaptureItemID, &captureItemDispatch)
@@ -88,33 +101,39 @@ func (c *CaptureHandler) StartCapture(hwnd win.HWND) error {
 		return errors.Wrap(err, "CreateForMonitor")
 	}
 
+	// Get Interface of IGraphicsCaptureItem
 	err = captureItemDispatch.PutQueryInterface(winrt.IGraphicsCaptureItemID, &c.graphicsCaptureItem)
 	if err != nil {
 		return errors.Wrap(err, "PutQueryInterface captureItemDispatch")
 	}
 
+	// Get Capture objects size
 	size, err := c.graphicsCaptureItem.Size()
 	if err != nil {
 		return errors.Wrap(err, "Size")
 	}
 
+	// Get object of Direct3D11CaptureFramePoolClass
 	ins, err := ole.RoGetActivationFactory(winrt.Direct3D11CaptureFramePoolClass, winrt.IDirect3D11CaptureFramePoolStaticsID)
 	if err != nil {
 		return errors.Wrap(err, "RoGetActivationFactory: IDirect3D11CaptureFramePoolStatics Class Instance")
 	}
 	defer ins.Release()
 
+	// Get Interface of Direct3D11CaptureFramePoolClass
 	var framePoolStatic *winrt.IDirect3D11CaptureFramePoolStatics
 	err = ins.PutQueryInterface(winrt.IDirect3D11CaptureFramePoolStaticsID, &framePoolStatic)
 	if err != nil {
 		return errors.Wrap(err, "PutQueryInterface: IDirect3D11CaptureFramePoolStaticsID")
 	}
 
+	// Create frame pool
 	c.framePool, err = framePoolStatic.Create(c.device, winrt.DirectXPixelFormat_B8G8R8A8UIntNormalized, 1, size)
 	if err != nil {
 		return errors.Wrap(err, "CreateFramePool")
 	}
 
+	// Set frame settings
 	var eventObject winrt.Direct3D11CaptureFramePoolVtbl
 	eventObject.Invoke = syscall.NewCallback(c.onFrameArrived)
 
@@ -128,6 +147,7 @@ func (c *CaptureHandler) StartCapture(hwnd win.HWND) error {
 		return errors.Wrap(err, "CreateCaptureSession")
 	}
 
+	// Start capturing
 	err = c.graphicsCaptureSession.StartCapture()
 	if err != nil {
 		return errors.Wrap(err, "StartCapture")
